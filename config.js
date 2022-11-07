33387d7a2c8371f51ba1d8b479e55c7230ac99e6
CbrWlf <50838230+CbrWlf@users.noreply.github.com>
2022-11-07 13:05:39 +0100

    Update config.js

--- config.js
+++ config.js
@@ -160,7 +160,7 @@ const CONFIG = {
 				},
 			],
 		},
-		/*{
+		{
 			icon: 'coffee',
 			id: '3',
 			links: [
@@ -179,7 +179,7 @@ const CONFIG = {
 				{
 					name: 'Slack',
 					link: 'https://www.slack.com',
-				},*/
+				},
 			],
 		},
 	],
